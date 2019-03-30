8d0a7ddd45a96ac6ef5cc02c20b85cdc981b94e6
Federico Moretti <hello@federicomoretti.it>
2019-03-30 20:58:44 +0100

    update gulpfile.js, package-lock.json, and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("styles", () => {
+  return gulp.src("./assets/scss/*.scss")
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"]
+    }))
+    .pipe(gulp.dest("./assets/css/"));
+});
+
+gulp.task("default", () => {
+
+});
